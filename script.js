3bd923b0d2eb38615c227fe55996344762d24261
Rickie Natividad <rickienatividad85@gmail.com>
2022-12-13 16:24:42 -0500

    adds points to player 1 current score with each dice roll. Rolling a 1 will erase current score

--- script.js
+++ script.js
@@ -4,10 +4,21 @@ const p1Score = document.querySelector(".player-1-score");
 const p2Current = document.querySelector("#p2");
 const p2Score = document.querySelector(".player-2-score");
 
+const dice = document.querySelector(".dice");
+
 const rollButton = document.querySelector(".roll");
 
 rollButton.addEventListener("click", function rollDice() {
   const random = Math.trunc(Math.random() * (7 - 1) + 1);
   console.log(random);
+
+  dice.setAttribute("src", `./assets/dice${random}.png`);
+
+  if (random !== 1) {
+    p1Current.textContent = Number(p1Current.textContent) + random;
+  } else if (random === 1) {
+    p1Current.textContent = 0;
+  }
+
   return random;
 });
