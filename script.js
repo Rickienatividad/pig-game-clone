c6587ae67063f65ac2e0cbd02cb3e02300f7cbfc
Rickie Natividad <rickienatividad85@gmail.com>
2022-12-13 17:56:13 -0500

    adds score to player 1 after clicking hold button. background colors swap.

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 const p1Current = document.querySelector("#p1");
 const p1Score = document.querySelector(".player-1-score");
+const p1Half = document.querySelector(".player-1-half");
+p1Half.style.backgroundColor = "rgb(244, 195, 244)";
 
 const p2Current = document.querySelector("#p2");
 const p2Score = document.querySelector(".player-2-score");
+const p2Half = document.querySelector(".player-2-half");
+p2Half.style.backgroundColor = "rgb(223, 126, 220)";
 
 const dice = document.querySelector(".dice");
 
 const rollButton = document.querySelector(".roll");
+const holdButton = document.querySelector(".hold");
 
 rollButton.addEventListener("click", function rollDice() {
   const random = Math.trunc(Math.random() * (7 - 1) + 1);
@@ -19,6 +24,20 @@ rollButton.addEventListener("click", function rollDice() {
   } else if (random === 1) {
     p1Current.textContent = 0;
   }
-
   return random;
+});
+
+holdButton.addEventListener("click", function p1AddScore() {
+  p1Score.textContent = p1Current.textContent;
+  p1Current.textContent = 0;
+  if (p1Half.style.backgroundColor === "rgb(223, 126, 220)") {
+    p1Half.style.backgroundColor = "rgb(244, 195, 244)";
+  } else if (p1Half.style.backgroundColor === "rgb(244, 195, 244)") {
+    p1Half.style.backgroundColor = "rgb(223, 126, 220";
+  }
+  if (p2Half.style.backgroundColor === "rgb(223, 126, 220)") {
+    p2Half.style.backgroundColor = "rgb(244, 195, 244)";
+  } else if (p2Half.style.backgroundColor === "rgb(244, 195, 244)") {
+    p2Half.style.backgroundColor = "rgb(223, 126, 220";
+  }
 });
