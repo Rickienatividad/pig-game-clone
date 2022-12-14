76c7e5bd66910cacce0f82f1aee3a837abb6f8a3
Rickie Natividad <rickienatividad85@gmail.com>
2022-12-14 16:13:54 -0500

    refactors code to switch active player

--- script.js
+++ script.js
@@ -8,36 +8,32 @@ const p2Score = document.querySelector(".player-2-score");
 const p2Half = document.querySelector(".player-2-half");
 p2Half.style.backgroundColor = "rgb(223, 126, 220)";
 
+//set current player
+let activePlayer = 1;
+
 const dice = document.querySelector(".dice");
 
 const rollButton = document.querySelector(".roll");
 const holdButton = document.querySelector(".hold");
+
+let currentScore;
+
+function switchPlayer() {
+  holdButton.addEventListener("click", function () {
+    let active = activePlayer === 1 ? (activePlayer = 2) : (activePlayer = 1);
+    activePlayer = active;
+    console.log(activePlayer);
+    return activePlayer;
+  });
+}
+switchPlayer();
 
 rollButton.addEventListener("click", function rollDice() {
   const random = Math.trunc(Math.random() * (7 - 1) + 1);
   console.log(random);
 
   dice.setAttribute("src", `./assets/dice${random}.png`);
-
-  if (random !== 1) {
-    p1Current.textContent = Number(p1Current.textContent) + random;
-  } else if (random === 1) {
-    p1Current.textContent = 0;
-  }
-  return random;
-});
-
-holdButton.addEventListener("click", function p1AddScore() {
-  p1Score.textContent = p1Current.textContent;
-  p1Current.textContent = 0;
-  if (p1Half.style.backgroundColor === "rgb(223, 126, 220)") {
-    p1Half.style.backgroundColor = "rgb(244, 195, 244)";
-  } else if (p1Half.style.backgroundColor === "rgb(244, 195, 244)") {
-    p1Half.style.backgroundColor = "rgb(223, 126, 220";
-  }
-  if (p2Half.style.backgroundColor === "rgb(223, 126, 220)") {
-    p2Half.style.backgroundColor = "rgb(244, 195, 244)";
-  } else if (p2Half.style.backgroundColor === "rgb(244, 195, 244)") {
-    p2Half.style.backgroundColor = "rgb(223, 126, 220";
-  }
+  document.querySelector(`#p${activePlayer}`).textContent =
+    Number(document.querySelector(`#p${activePlayer}`).textContent) +
+    Number(random);
 });
